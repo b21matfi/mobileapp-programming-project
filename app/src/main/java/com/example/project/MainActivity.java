b5e376c9c62c735b4421cec230870de8ac85dad4
b21matfi <b21matfi@student.his.se>
2022-05-23 14:10:51 +0200

    Made a JsonTask java class.

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -1,11 +1,13 @@
 package com.example.project;
 
 import android.os.Bundle;
-
+import com.example.networking.JsonTask;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+    public JsonTask jsontask;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
